9231ec855df4c08390aa6ec99cdb294e09b3d583
Brian Stewart <bstewart@usnews.com>
2019-03-04 17:18:50 -0500

    Initial commit 2: the restartening

---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.md')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'SQLAlchemy',
+]
+
+tests_require = [
+    'pytest',
+    'pytest-cov',
+]
+
+setup(
+    name='marmot',
+    version='0.0',
+    description='Marmot',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = marmot:main',
+        ],
+        'console_scripts': [
+            'initialize_marmot_db = bin.initializedb:main',
+        ],
+    },
+)
