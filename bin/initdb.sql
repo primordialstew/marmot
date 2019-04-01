a270dc7317b9e9e4d6ca5910bf31c588a7fafb8e
Brian Stewart <bstewart@usnews.com>
2019-04-01 13:26:01 -0400

    Update database creation script

--- bin/initdb.sql
+++ bin/initdb.sql
@@ -1,8 +1,6 @@
-DROP DATABASE marmot;
-DROP ROLE marmot_alembic;
-DROP ROLE marmot_admin;
-
+-- Before we can begin using Alembic, we must create the roles and database
 
 CREATE ROLE marmot_admin WITH SUPERUSER;
 CREATE ROLE marmot_alembic WITH LOGIN ROLE marmot_admin;
 CREATE DATABASE marmot WITH OWNER marmot_alembic;
+ALTER SCHEMA public OWNER TO marmot_alembic;
